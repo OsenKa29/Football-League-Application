1683d84fcec4c100a25c7b0e5846d8b38783c174
OsenKa29 <75950443+OsenKa29@users.noreply.github.com>
2022-03-08 00:13:34 +0300

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,47 @@
+package com.Classing;
+
+public abstract class Player {
+
+    private String name = null;
+    private int age;
+    private double height;
+    private int weight;
+    private int number;
+    private String position = null;
+
+    public Player(String name, int age, double height,int weight, int number, String position) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+        this.number = number;
+        this.position = position;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public abstract  void printPlayer();
+}
